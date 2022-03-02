2ee5f19eabee0c136a035321335f6f2619640531
Kaneki316 <rabindangol12345@gmail.com>
2022-03-02 16:36:42 +0545

    control changes

--- Scenes/control.js
+++ Scenes/control.js
@@ -4,17 +4,22 @@ class control extends Phaser.Scene {
     }
     preload() {
         this.load.image("background", "assets/background/bg-preview-big.png");
+        // this.load.image("arrowkey1", "assests1/arrowkey1.png");
+        this.load.bitmapFont("pixelFont", "assests1/font/font.png", "assests1/font/font.xml");
     }
     create() {
         this.bg2 = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.bg2.setOrigin(0, 0);
-        var view = this.add.text(0, 0, "Spacebar=> Shoot")
-        var text = this.add.text(200, 200, "Back");
-        text.setInteractive(    //for making text interractive
+        //let key = this.add.image(config.width / 3, config.height / 3, "arrowkey1");
+        //key.setScale(0.15);
+        var texttitle = this.add.bitmapText(130, 50, "pixelFont", "How To play", 55);
+
+        // var back = this.add.bitmapText(200, 400, "pixelFont", "BACK", 30);
+        back.setInteractive(    //for making text interractive
             {
                 useHandCursor: true
             });
-        text.on('pointerdown', () => this.clickButton(
+        back.on('pointerdown', () => this.clickButton(
             this.scene.switch("loadscreen")
         ));
         /*   var text = this.add.text(200, 240, "Controls");
@@ -25,6 +30,6 @@ class control extends Phaser.Scene {
 
     }
     update() {
-        this.bg2.tilePositionY -= 0.5;
+        //this.bg2.tilePositionY -= 0.5;
     }
 }
